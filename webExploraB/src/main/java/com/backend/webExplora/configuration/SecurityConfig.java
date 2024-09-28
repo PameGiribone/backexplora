336397779f2ee5ab8e38ac5baa0b5557def0c460
PameGiribone <pame.giribone@hotmail.com>
2024-09-27 22:50:48 -0300

    corss

--- webExploraB/src/main/java/com/backend/webExplora/configuration/SecurityConfig.java
+++ webExploraB/src/main/java/com/backend/webExplora/configuration/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Permitir origen específico
+        configuration.setAllowedOrigins(List.of("https://accurate-spontaneity-production.up.railway.app/")); // Permitir origen específico
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Permitir métodos HTTP
         configuration.setAllowedHeaders(List.of("*")); // Permitir todos los headers
         configuration.setAllowCredentials(true); // Permitir credenciales (si se requiere)
